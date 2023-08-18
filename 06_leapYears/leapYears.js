166e50a529cac869dabb6c18293d47c424f686e4
Dennis Esguerra <137248049+Dexmi25@users.noreply.github.com>
2023-08-18 20:10:33 +0800

    Answer

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+// If the year is divisible by 4, it's a potential leap year.
+    return year % 4 === 0 
+
+// If the year is divisible by 100 but not by 400, it's not a leap year.
+// If the year is divisible by 400, it's a leap year.
+    && (year % 100 !== 0
+    || year % 400 === 0);
 
 };
 
