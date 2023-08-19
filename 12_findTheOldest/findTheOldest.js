ae1d4969815c37ef662c74047ade1d1453a35b41
Dennis Esguerra <137248049+Dexmi25@users.noreply.github.com>
2023-08-19 13:17:57 +0800

    Answer

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
 
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+};
+
+// Make a function that calculates the age with the arguments birth and death
+// this represents the year of birth and year of death or the current year if not provided
+const getAge = function (birth, death) {
+
+// First check if the year of death is not provided
+    if (!death) {
+
+// then if not provided it will sets death to the current year by using
+// the new Date().getFullYear() function.
+        death = new Date().getFullYear();
+    }
+
+    return death - birth;
 };
 
 // Do not edit below this line
