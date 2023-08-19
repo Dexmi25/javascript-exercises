52bcb2830b161977591fe0812f9ed90479550a87
Dennis Esguerra <137248049+Dexmi25@users.noreply.github.com>
2023-08-19 11:52:48 +0800

    Answer

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+// First convert all string .toLowerCase then .replace all character
+// to empty string that is not lowercase letter or digit.
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+// then the return value will be split into array of individual character
+// reverses the order of the array
+// then joins the reversed array back into a single string
+// lastly compares this reverse string with the original
+// if they are equal return true if not false.
+    return processedString.split("").reverse().join("") == processedString;
 
 };
 
