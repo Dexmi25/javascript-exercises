e26dc899e29af957716c3719c084dd813d435e9d
Dennis Esguerra <137248049+Dexmi25@users.noreply.github.com>
2023-08-19 12:57:30 +0800

    Answer

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+
+// .map(); Take a single argument a callback function
+// that will be applied to each element of the array;
+    return array.map((book) => book.title);
 
 };
 
